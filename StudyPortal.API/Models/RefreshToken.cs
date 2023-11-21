c0a252a37c884246bb73a06bc74e86c951ab7d96
djoob20 <djoob20@gmail.com>
2023-11-21 22:43:47 +0100

    deleted projects

--- StudyPortal.API/Models/RefreshToken.cs
+++
@@ -1,8 +0,0 @@
-namespace StudyPortal.API.Models;
-
-public class RefreshToken
-{
-    public string Token { get; set; }
-    public DateTime Created { get; set; }
-    public DateTime Expires { get; set; }
-}
